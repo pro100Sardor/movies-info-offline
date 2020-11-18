39964d32fcc02e55bfa9efdb4fe92c4bf000d368
pro100Sardor <sardorbek.juraqulov@gmail.com>
2020-11-18 21:25:26 +0500

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 var elPageMain = $_ ('#pageMain');
 
 function createCinemaItem (itemFeatureTitle, itemFeatureText) {
-  var item = document.createElement('li');
-  item.setAttribute('class', 'mb-4');
+    var item = document.createElement('li');
+    item.setAttribute('class', 'mb-4');
 
-  var cinemaItemFeatureTitle = document.createElement('span');
-  cinemaItemFeatureTitle.setAttribute('class', 'font-weight-bold');
-  cinemaItemFeatureTitle.textContent = itemFeatureTitle;
+    var cinemaItemFeatureTitle = document.createElement('span');
+    cinemaItemFeatureTitle.setAttribute('class', 'font-weight-bold');
+    cinemaItemFeatureTitle.textContent = itemFeatureTitle;
 
-  item.appendChild(cinemaItemFeatureTitle);
-  item.innerHTML += itemFeatureText;
-  return item;
-}
+    item.appendChild(cinemaItemFeatureTitle);
+    item.innerHTML += itemFeatureText;
+    return item;
+  }
 
 kinolar.forEach(function(kino) {
   var cinemaList = document.createElement('ul');
@@ -25,4 +25,25 @@ kinolar.forEach(function(kino) {
 
   elPageMain.appendChild(cinemaList);
 });
+
+// normalizedMovies.splice(0, 10).forEach(function(movie) {
+//   var cinemaList = createElement('ul', 'movies-list list-unstyled mb-4 bg-white rounded pt-3 shadow');
+
+//   cinemaList.appendChild(createCinemaItem('№: ', movie.id));
+//   cinemaList.appendChild(createCinemaItem('Title: ', movie.title));
+//   cinemaList.appendChild(createCinemaItem('Year: ', movie.year));
+//   cinemaList.appendChild(createCinemaItem('Category: ', movie.categories));
+//   cinemaList.appendChild(createCinemaItem('Summary: ', movie.summary));
+//   cinemaList.appendChild(createCinemaItem('IMDB id: ', movie.imdbId));
+//   cinemaList.appendChild(createCinemaItem('IMDB Reiting: ', movie.imdbRating));
+//   cinemaList.appendChild(createCinemaItem('Runtime: ', movie.runtime));
+//   cinemaList.appendChild(createCinemaItem('Language: ', movie.language));
+//   var cinemaTriller = document.createElement('a');
+//   cinemaTriller.href = `http://youtube.com/watch?v=${movie.youtubeId}`;
+//   cinemaTriller.target = '_blank';
+//   cinemaTriller.textContent = movie.youtubeId;
+//   cinemaList.appendChild(createCinemaItem('Triller: ', cinemaTriller));
+
+//   elPageMain.appendChild(cinemaList);
+// });
 
