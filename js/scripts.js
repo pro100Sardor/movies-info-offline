a3335aabb1a815395f7e0ee2dcc31e0ba80982fd
pro100Sardor <sardorbek.juraqulov@gmail.com>
2020-11-18 16:00:18 +0500

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,21 @@
 var elPageMain = $_ ('#pageMain');
 
+function createCinemaItem (itemFeatureTitle, itemFeatureText) {
+  var item = document.createElement('li');
+  item.setAttribute('class', 'mb-4');
+
+  var cinemaItemFeatureTitle = document.createElement('span');
+  cinemaItemFeatureTitle.setAttribute('class', 'font-weight-bold');
+  cinemaItemFeatureTitle.textContent = itemFeatureTitle;
+
+  item.appendChild(cinemaItemFeatureTitle);
+  item.innerHTML += itemFeatureText;
+  return item;
+}
+
 kinolar.forEach(function(kino) {
-  var cinemaContainer = document.createElement('div');
-  cinemaContainer.setAttribute('class', 'd-flex w-50 mb-4');
-
   var cinemaList = document.createElement('ul');
-  cinemaList.setAttribute('class', 'list-unstyled ml-4');
-
-  function createCinemaItem (itemFeatureTitle, itemFeatureText) {
-    var item = document.createElement('li');
-    item.setAttribute('class', 'mb-4');
-
-    var cinemaItemFeatureTitle = document.createElement('span');
-    cinemaItemFeatureTitle.setAttribute('class', 'font-weight-bold');
-    cinemaItemFeatureTitle.textContent = itemFeatureTitle;
-
-    item.appendChild(cinemaItemFeatureTitle);
-    item.innerHTML += itemFeatureText;
-    return item;
-  }
+  cinemaList.setAttribute('class', 'list-unstyled w-50 mb-4');
 
   var keys = Object.keys(kino);
   cinemaList.appendChild(createCinemaItem(`${keys[0]}: `, kino.title));
@@ -26,8 +23,6 @@ kinolar.forEach(function(kino) {
   cinemaList.appendChild(createCinemaItem(`${keys[2]}: `, kino.cast));
   cinemaList.appendChild(createCinemaItem(`${keys[3]}: `, kino.genres));
 
-  cinemaContainer.appendChild(cinemaList);
-
-  elPageMain.appendChild(cinemaContainer);
+  elPageMain.appendChild(cinemaList);
 });
 
